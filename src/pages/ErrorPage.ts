d8d8e0f6fc67aeb915d77c71b870407ea9f7d8f5
delvikdan <delvikdan@gmail.com>
2025-10-10 14:45:05 +0300

    Fix logout link logic, modify error page props

--- src/pages/ErrorPage.ts
+++ src/pages/ErrorPage.ts
@@ -3,13 +3,16 @@ import { Error } from "@/components/Error";
 
 export type ErrorPageProps = {
   code: string;
+  status: string;
 };
 
 export class ErrorPage extends Block {
-  constructor(props: ErrorPageProps = { code: "404" }) {
+  constructor(
+    props: ErrorPageProps = { code: "404", status: "Не туда попали" }
+  ) {
     const error: Error = new Error({
       code: props.code,
-      status: "Мы уже фиксим",
+      status: props.status,
       href: "/messenger",
       text: "Назад к чатам",
       isRouterLink: true,
